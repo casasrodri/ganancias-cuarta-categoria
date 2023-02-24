6925026b5035b2dc924a63e5183ddbc1c176c499
Rodri Casas <cr.rodrigocasas@gmail.com>
2023-02-24 15:38:24 -0300

    cambios tramo 2 - submodules

--- deducciones/incremental/tablas_tramo2.py
+++ deducciones/incremental/tablas_tramo2.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 class TablasTramo2:
     def __init__(self) -> None:
-        self.data = pd.read_excel('./bases-ganancias-2022/tablas_afip.xlsx')
+        self.data = pd.read_excel('./bases-ganancias-2022/tablas_afip_tramo2.paquet')
 
     def deduccion(self, mes, sueldo):
         df = self.data
         filtro_mes = df['ini'].le(mes) & df['fin'].ge(mes)
-        filtro_rem = df['sueldo'].ge(sueldo)
+        filtro_rem = df['base'].ge(sueldo)
         primer_row = df[filtro_mes & filtro_rem].head(1)
         if len(primer_row) == 0:
             raise ValueError(f'No es posible obtener una deducción en la tabla para el importe {sueldo} en el mes {mes}.')
