6997fe997a8728fa1ce27ef2b733da52ae698bc5
Pau Griffa <pau.griffa10@gmail.com>
2023-04-17 21:14:42 -0300

    ult cambios

--- deducciones/incremental/tablas_tramo2.py
+++ deducciones/incremental/tablas_tramo2.py
@@ -3,7 +3,7 @@ from helpers.bases import tabla
 class TablasTramo2:
     @staticmethod
     def deduccion(mes, sueldo):
-        df = tabla('tablas_afip_tramo2')
+        df = tabla('tablas_afip_tramo2').sort_values(by="base")
         filtro_mes = df['ini'].le(mes) & df['fin'].ge(mes)
         filtro_rem = df['base'].ge(sueldo)
         primer_row = df[filtro_mes & filtro_rem].head(1)
